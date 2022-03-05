6fd4b80ed976c43f57f78c92de516e04227e4b55
Dylan Kriegman <dylan.kriegman@netapp.com>
2022-03-05 13:22:38 -0700

    setup ejs and homepage routing

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+var express = require('express'); //Ensure our express framework has been added
+var app = express();
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+
+app.get('/',function(req,res) {
+	res.render('pages/homePage');
+});
+
+app.listen(3000);
+console.log('3000 is the magic port');
